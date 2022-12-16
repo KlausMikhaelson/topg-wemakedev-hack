d8574392fa4faa9b23d21d428d4c2d370777bbb4
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-12-16 12:47:17 +0530

    fixing deployment error

--- pages/Component/Texture.tsx
+++
@@ -1,9 +0,0 @@
-// import { Land } from "./Images";
-// // @ts-ignore
-// import {TextureLoader} from "three/src/loaders/TextureLoader"
-
-// const LandTexture = new TextureLoader().load(Land)
-
-// export{
-//     LandTexture
-// }
