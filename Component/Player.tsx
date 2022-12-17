a7db62591c308172e9dcfc179ccb0aea76de11c1
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-12-17 21:32:23 +0530

    added kunals stuff

--- Component/Player.tsx
+++ Component/Player.tsx
@@ -1,44 +1,45 @@
 import { useRouter } from 'next/router'
-// @ts-ignore
+    // @ts-ignore 
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { Physics, useBox, useCompoundBody, usePlane } from '@react-three/cannon'
 import { useFrame, useLoader, useThree } from "@react-three/fiber"
 import { OrbitControls, useAnimations, useHelper } from "@react-three/drei";
 import React, { forwardRef, useEffect, useRef } from "react";
 import { useInput } from "../hooks/Keyboard";
-// @ts-ignore
+    // @ts-ignore 
 import * as THREE from "three"
-import { io } from 'socket.io-client';
+// @ts-ignore
+import { NearestFilter, RepeatWrapping } from "three"
+// @ts-ignore
+import { TextureLoader } from "three/src/loaders/TextureLoader"
 
 
 const walkdirection = new THREE.Vector3()
-let rotateAngle = new THREE.Vector3(0,1,0);
+let rotateAngle = new THREE.Vector3(0, 1, 0);
 let rotateQuarternion = new THREE.Quaternion();
 let cameraTarget = new THREE.Vector3()
-
-// @ts-ignore
-
-const directions = ({forward, backward, left, right}) => {
+    // @ts-ignore 
+const directions = ({ forward, backward, left, right }) => {
     var directions = 0;
 
     // diagonals
 
-    if(forward) {
-        if(left) {
+    if (forward) {
+        if (left) {
             directions = Math.PI / 4;
-        } else if(right) {
+        } else if (right) {
             directions = -Math.PI / 4;
         }
     } else if (backward) {
-        if(left) {
+        if (left) {
             directions = Math.PI / 4 + Math.PI / 2;
-        } else if(right) {
-            directions = -Math.PI / 4 - Math.PI / 2; 
+        } else if (right) {
+            directions = -Math.PI / 4 - Math.PI / 2;
         } else {
             directions = Math.PI;
         }
         // left
-    } else if(left) {
+    } else if (left) {
         directions = Math.PI / 2;
         // right
     } else if (right) {
@@ -49,12 +50,43 @@ const directions = ({forward, backward, left, right}) => {
 }
 
 
+// @ts-ignore
+function getRandomArbitrary(low, high) {
+    return Math.random() * (high - low) + low;
+}
 
-const Player: React.FC = () => { 
+var itemPos = [getRandomArbitrary(-100, 100), 1, getRandomArbitrary(-100, 100)];
+export const GiftModel: React.FC = () => {
+    const KunalMap = useLoader(TextureLoader, './kunals.png')
+    // colorMap.wrapS = RepeatWrapping
+    // colorMap.wrapT = RepeatWrapping
+    // colorMap.repeat.set(1,150)
 
-    const {forward, backward, left, right, jump}  = useInput()
+    // const modelGift = useLoader(GLTFLoader, "./kunal2.glb")
+
+    const [ref] = useBox(() => ({
+        // type: 'static',
+        // mass: 1,
+        // @ts-ignore 
+        position: itemPos
+    }))
+
+    return (
+        <>
+            <mesh ref={ref} rotation-x={Math.PI * -0.5} scale={2}>
+                <ambientLight />
+                <boxGeometry />
+                <meshStandardMaterial map={KunalMap} />
+            </mesh>
+        </>
+    )
+}
+
+
+const Player: React.FC = () => {
+    const { forward, backward, left, right, jump } = useInput()
     const modelPlayer = useLoader(GLTFLoader, "./player3.glb")
-    const {actions} = useAnimations(modelPlayer.animations, modelPlayer.scene)
+    const { actions } = useAnimations(modelPlayer.animations, modelPlayer.scene)
 
     const currentAction = useRef("")
     const controlsref = useRef<typeof OrbitControls>();
@@ -69,21 +101,21 @@ const Player: React.FC = () => {
         cameraTarget.y = modelPlayer.scene.position.y + 2;
         cameraTarget.z = modelPlayer.scene.position.z;
         // @ts-ignore
-        if(controlsref.current) controlsref.current.target = cameraTarget;
+        if (controlsref.current) controlsref.current.target = cameraTarget;
     }
-  
+
     useEffect(() => {
         let action = ""
 
-        if(forward || backward || left || right) {
+        if (forward || backward || left || right) {
             action = "walk"
-        } else if(jump) {
-            action="walk"
+        } else if (jump) {
+            action = "idle"
         } else {
-            action="idle"
+            action = "idle"
         }
 
-        if(currentAction.current != action) {
+        if (currentAction.current != action) {
             const nextAction = actions[action];
             const current = actions[currentAction.current]
             current?.fadeOut(0.2);
@@ -95,7 +127,7 @@ const Player: React.FC = () => {
 
 
     useFrame((state, delta) => {
-        if(currentAction.current == "walk") {
+        if (currentAction.current == "walk") {
             let angleYcameraDirection = Math.atan2(
                 camera.position.x - modelPlayer.scene.position.x,
                 camera.position.z - modelPlayer.scene.position.z
@@ -106,7 +138,7 @@ const Player: React.FC = () => {
                 left,
                 right
             });
-// rotating
+            // rotating
             rotateQuarternion.setFromAxisAngle(
                 rotateAngle,
                 angleYcameraDirection + newDirections
@@ -130,35 +162,39 @@ const Player: React.FC = () => {
 
     const router = useRouter()
     var playerPos = [Math.round(modelPlayer.scene.position.x), Math.round(modelPlayer.scene.position.y), Math.round(modelPlayer.scene.position.z)];
-    // var avItemPos = [Math.round(itemPos[0]), Math.round(itemPos[1]), Math.round(itemPos[2])]
+    var avItemPos = [Math.round(itemPos[0]), Math.round(itemPos[1]), Math.round(itemPos[2])]
 
     console.log(playerPos);
-    // socket.emit('move', {
-    //     position: playerPos,
-    // })
-    // console.log(avItemPos);
+    console.log(avItemPos);
+    if ((playerPos[0] == avItemPos[0] && playerPos[1] == avItemPos[1]) ||
+        (playerPos[1] == avItemPos[1] && playerPos[2] == avItemPos[2]) ||
+        (playerPos[0] == avItemPos[0] && playerPos[2] == avItemPos[2])) {
+
+        console.log("You win")
+        // router.push('/gameover');
+    }
 
     const [ref] = useBox(
         () => ({
-        //   collisionFilterGroup: GROUP_BODY,
-        //   collisionFilterMask: GROUP_GROUND,
-        //   linearDamping: 0.4,
-        type: "Kinematic",
-          mass: 1,
-        //   ...props,
+            //   collisionFilterGroup: GROUP_BODY,
+            //   collisionFilterMask: GROUP_GROUND,
+            linearDamping: 0.4,
+            mass: 1,
+            //   ...props,
         }),
         // ref,
-      )
-
-    return(
-      <>
-      <mesh ref={ref}>
-        {/* @ts-ignore */}
-        <OrbitControls ref={controlsref} maxPolarAngle={Math.PI/2.5} enableZoom={false}/>
-        <primitive object={modelPlayer.scene} scale={0.5}/>
-      </mesh>
-      </>
     )
-  }
 
-  export default Player;
+    return (
+        <>
+        {/* @ts-ignore */}
+            <mesh ref={ref}>
+        {/* @ts-ignore */}
+                <OrbitControls ref={controlsref} maxPolarAngle={Math.PI / 2.5} enableZoom={false} />
+                <primitive scale={0.5} object={modelPlayer.scene} />
+            </mesh>
+        </>
+    )
+}
+
+export default Player;
